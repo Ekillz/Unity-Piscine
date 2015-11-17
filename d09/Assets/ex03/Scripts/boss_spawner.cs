0c8b9e1cf89a91d411c15a8bfb0202fa4b80bceb
Emil MAMMADZADA <emammadz@e3r9p16.42.fr>
2015-11-17 14:02:59 +0100

    added finished d09 unity fps shooter

---
+++ d09/Assets/ex03/Scripts/boss_spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boss_spawner : MonoBehaviour {
+
+	public GameObject enemy;
+	public bool spawn_start;
+	// Use this for initialization
+	void Start () {
+		spawn_start = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wave_manager.instance.wave_start && !spawn_start && wave_manager.instance.level % 3 == 0)
+			StartCoroutine (spawn ());
+	}
+	
+	IEnumerator spawn()
+	{
+		spawn_start = true;
+		Instantiate (enemy, transform.position, Quaternion.identity);
+		wave_manager.instance.boss_alive = true;
+		wave_manager.instance.current_zombies++;
+		spawn_start = false;
+		yield return 0;
+	}
+}
